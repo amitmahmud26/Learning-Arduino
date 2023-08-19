65b7952412d10447590dd12ff9a7fe7254ccf260
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-08-19 22:36:18 +0600

    RFID punching info

--- RFID/RFID-info.ino
+++ RFID/RFID-info.ino
@@ -1,26 +1,16 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 
 #define SS_PIN 10  // Slave Select pin
 #define RST_PIN 9  // Reset pin
 
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-
-// Define the UID of the punched card
-String punchedCardUID = "dc7fba38";
 
 void setup() {
   Serial.begin(9600);  // Initialize serial communication
   SPI.begin();  // Initiate SPI bus
   mfrc522.PCD_Init();  // Initiate MFRC522
-  lcd.init();  // Initialize the LCD
-  lcd.backlight();  // Turn on the backlight
-  lcd.setCursor(0, 0);
-  lcd.print("Scan an RFID card");
+  Serial.println("Scan an RFID card to punch it.");
 }
 
 void loop() {
@@ -34,18 +24,7 @@ void loop() {
     }
 
     // Print the UID
-    Serial.println(uidStr);
-
-    // Check if the scanned UID matches the specific card's UID
-    if (uidStr == punchedCardUID) {
-      lcd.clear();
-      lcd.setCursor(3, 0);
-      lcd.print("Card Punched");
-    } else {
-      lcd.clear();
-      lcd.setCursor(3, 0);
-      lcd.print("Not Punched");
-    }
+    Serial.println("Card Punched - UID: " + uidStr);
 
     mfrc522.PICC_HaltA();  // Halt the current card
     mfrc522.PCD_StopCrypto1();  // Stop encryption on the card
